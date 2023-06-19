b3212ab9efe709cace470396636c5e69b4444d04
Surayya-code <surayyaah@code.edu.az>
2023-06-19 07:24:55 +0400

    getStarted

--- app/src/main/java/com/example/techecapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/techecapp/ui/SplashFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.techecapp.R
 import com.example.techecapp.databinding.FragmentSplashBinding
 
@@ -24,6 +25,9 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.getStartedBtn.setOnClickListener(){
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }
     }
 
     override fun onDestroyView() {
